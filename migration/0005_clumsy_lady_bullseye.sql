0f6d33da95349d7056fb18f97a36a29901b4e298
cmyang1218 <laurence.yang1218@gmail.com>
2025-05-13 21:39:51 +0800

    update migration file

--- migration/0005_clumsy_lady_bullseye.sql
+++ migration/0005_clumsy_lady_bullseye.sql
@@ -2,7 +2,7 @@ CREATE TYPE "public"."user_role" AS ENUM('user', 'admin');--> statement-breakpoi
 CREATE TABLE "refresh_token" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"userId" integer NOT NULL,
-	"token_hash" varchar(255) NOT NULL,
+	"token" varchar(255) NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"expired_at" timestamp NOT NULL
 );
@@ -20,4 +20,5 @@ CREATE TABLE "user" (
 ALTER TABLE "service" DROP CONSTRAINT "service_pool_id_ip_pool_id_fk";
 --> statement-breakpoint
 ALTER TABLE "refresh_token" ADD CONSTRAINT "refresh_token_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+CREATE UNIQUE INDEX "token_idx" ON "refresh_token" USING btree ("token");--> statement-breakpoint
 ALTER TABLE "service" ADD CONSTRAINT "service_pool_id_ip_pool_id_fk" FOREIGN KEY ("pool_id") REFERENCES "public"."ip_pool"("id") ON DELETE cascade ON UPDATE no action;
